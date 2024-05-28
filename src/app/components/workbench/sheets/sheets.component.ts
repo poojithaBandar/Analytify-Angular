65ad3d0dd4b3bf26a98e8901234d2280800a9201
vjhansi <vjhansi@vjhansi-sa.stratapps.com>
2024-05-28 13:43:40 +0530

    Table preview done in sheet

--- src/app/components/workbench/sheets/sheets.component.ts
+++ src/app/components/workbench/sheets/sheets.component.ts
@@ -12,6 +12,9 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
+interface TableRow {
+  [key: string]: any;
+}
 @Component({
   selector: 'app-sheets',
   standalone: true,
@@ -22,6 +25,23 @@ import {
 export class SheetsComponent {
   tableColumnsData = [] as any;
   draggedtables = [] as any;
+  draggedColumns = [] as any;
+  draggedRows = [] as any;
+  dimentions = [] as any;
+  measurments = [] as any;
+  columnData = [] as any;
+  rowData = [] as any;
+  tableName: any;
+  schemaName: any;
+  table_alias: any;
+  draggedColumnsData = [] as any;
+  draggedRowsData = [] as any;
+  tablePreviewColumn = [] as any;
+  tablePreviewRow = [] as any;
+  rowdataPreview = [] as any;
+  tableData: TableRow[] = [];
+  displayedColumns: string[] = [];
+ 
   constructor(private workbechService:WorkbenchService){   
   }
 
@@ -32,7 +52,7 @@ export class SheetsComponent {
   columnsData(){
     const obj={
       "db_id":"182",
-      "queryset_id":"35",
+      "queryset_id":"254",
   }
     this.workbechService.getColumnsData(obj).subscribe({next: (responce:any) => {
           console.log(responce);
@@ -45,10 +65,56 @@ export class SheetsComponent {
     )
   }
 
+  dataExtraction(){
+    this.tablePreviewColumn = [];
+    this.tablePreviewRow = [];
+    this.rowdataPreview = [];
+    this.tableData = [];
+      const obj={
+          "database_id":182,
+          "queryset_id":254,
+          "col":this.draggedColumnsData,
+          "row":this.draggedRowsData,
+          "filter_id": [60]
+      }
+    this.workbechService.getDataExtraction(obj).subscribe({next: (responce:any) => {
+          console.log(responce);
+          this.tablePreviewColumn = responce.data[0].col;
+          this.tablePreviewRow = responce.data[0].row;
+          console.log(this.tablePreviewColumn);
+          console.log(this.tablePreviewRow);
+      
+          const rowCount = this.tablePreviewColumn[0].result_data.length;
+      
+          // Extract column names
+          this.displayedColumns = this.tablePreviewColumn.map((col:any) => col.column).concat(this.tablePreviewRow.map((row:any) => row.column));
+      
+          // Create table data
+          for (let i = 0; i < rowCount; i++) {
+            const row: TableRow = {};
+            this.tablePreviewColumn.forEach((col:any) => {
+              row[col.column] = col.result_data[i];
+            });
+            this.tablePreviewRow.forEach((rowData:any) => {
+              row[rowData.column] = rowData.result_data[i];
+            });
+            this.tableData.push(row);
+         
+        }
+      console.log(this.tableData);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      }
+    )
+  }
     drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
+     // this.draggedtables = [];
+     // this.draggedColumns = [];
+   // if (event.previousContainer === event.container) {
+   //   moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    //} else {
       console.log('Transfering item to new container')
       // transferArrayItem(event.previousContainer.data,
       //                   event.container.data,
@@ -74,8 +140,10 @@ export class SheetsComponent {
         }
       }
       this.draggedtables.splice(event.currentIndex, 0, element);
-     }
-     this.columnsData();
+      
+      console.log(this.draggedtables);
+    // }
+    // this.columnsData();
     /* if(this.draggedtables.length > 1){
       const obj ={
         database_id : this.databaseId,
@@ -95,6 +163,47 @@ export class SheetsComponent {
         })
      }*/
   }
+  tableNameMethod(schemaname:any,tablename:any,tableAlias:any){
+    this.schemaName = '';
+    this.tableName = '';
+    this.table_alias = '';
+  this.schemaName = schemaname;
+  this.tableName = tablename;
+  this.table_alias = tableAlias;
+  }
+  columndrop(event: CdkDragDrop<string[]>){
+    console.log(event)
+    let item: any = event.previousContainer.data[event.previousIndex];
+    let copy: any = JSON.parse(JSON.stringify(item));
+    let element: any = {};
+    for (let attr in copy) {
+      if (attr == 'title') {
+        element[attr] = copy[attr];
+      } else {
+        element[attr] = copy[attr];
+      }
+    }
+    this.draggedColumns.splice(event.currentIndex, 0, element);
+    this.draggedColumnsData.push([this.schemaName,this.tableName,this.table_alias,element.column,element.data_type,""])
+    this.dataExtraction();
+  }
+  rowdrop(event: CdkDragDrop<string[]>){
+    let item: any = event.previousContainer.data[event.previousIndex];
+    let copy: any = JSON.parse(JSON.stringify(item));
+    let element: any = {};
+    for (let attr in copy) {
+      if (attr == 'title') {
+        element[attr] = copy[attr];
+      } else {
+        element[attr] = copy[attr];
+      }
+    }
+    this.draggedRows.splice(event.currentIndex, 0, element);
+    this.draggedRowsData.push([this.schemaName,this.tableName,this.table_alias,element.column,element.data_type,""])
+    this.dataExtraction();
+  }
+
+
   genders = [
     {id: 1,name: 'Texas'},
     {id: 2,name: 'Georgia'},
--- src/app/components/workbench/workbench.service.ts
+++ src/app/components/workbench/workbench.service.ts
@@ -28,12 +28,32 @@ export class WorkbenchService {
   getTableData(obj:any){
     return this.http.put<any>(`${environment.apiUrl}/get_table_relationship/`+this.accessToken,obj)
   }
-
+  getdatabaseConnectionsList(){
+    const currentUser = localStorage.getItem( 'currentUser' );
+    this.accessToken = JSON.parse( currentUser! )['Token'];
+    return this.http.get<any>(`${environment.apiUrl}/connection_list/`+this.accessToken)
+  }
+  getTablesFromConnectedDb(id:any){
+    const currentUser = localStorage.getItem( 'currentUser' );
+    this.accessToken = JSON.parse( currentUser! )['Token'];
+    return this.http.get<any>(`${environment.apiUrl}/tables_list/`+this.accessToken+'/'+id)
+  }
   getColumnsData(obj:any){
     const currentUser = localStorage.getItem( 'currentUser' );
     console.log(JSON.parse( currentUser!))
     this.accessToken = JSON.parse( currentUser! )['Token'];
     return this.http.post<any>(`${environment.apiUrl}/columnextracting/`+this.accessToken,obj);
+  }
+  executeQuery(obj:any){
+    return this.http.post<any>(`${environment.apiUrl}/custom_query/`+this.accessToken,obj);
+  }
+
+  joiningTables(obj:any){
+    return this.http.post<any>(`${environment.apiUrl}/tables_joining/`+this.accessToken,obj);
+
+  }
+  getTableJoiningData(obj:any){
+    return this.http.post<any>(`${environment.apiUrl}/query_data/`+this.accessToken,obj);
   }
   getDataExtraction(obj:any){
     const currentUser = localStorage.getItem( 'currentUser' );
