e63317178dfdcfd719285f7a424551c24db6f940
mn-stratapps <mnikhil@stratapps.com>
2024-06-13 16:26:21 +0530

    Added UI filter UI change in datasiurce page

--- src/app/components/workbench/sheetsdashboard/sheetsdashboard.component.ts
+++ src/app/components/workbench/sheetsdashboard/sheetsdashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sheetsdashboard',
   standalone: true,
-  imports: [],
+  imports: [NgbModule],
   templateUrl: './sheetsdashboard.component.html',
   styleUrl: './sheetsdashboard.component.scss'
 })
