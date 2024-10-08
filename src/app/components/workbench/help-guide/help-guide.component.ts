0b6590c0601fa8d00cf8c5b93cf1137036736337
mn-stratapps <mnikhil@stratapps.com>
2024-10-08 11:16:21 +0530

    Help-guide component

---
+++ src/app/components/workbench/help-guide/help-guide.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-guide',
+  standalone: true,
+  imports: [],
+  templateUrl: './help-guide.component.html',
+  styleUrl: './help-guide.component.scss'
+})
+export class HelpGuideComponent {
+
+}
