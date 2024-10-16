622e23da3bfcd3011bdb582964ce9cb817aadb13
rajeshkumar-stratapps <rajeshraikode@stratapps.com>
2024-10-16 17:45:26 +0530

    Database-content, Add index

--- src/app/components/workbench/help-guide/help-guide.component.ts
+++ src/app/components/workbench/help-guide/help-guide.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from '../../../shared/sharedmodule';
 
 @Component({
   selector: 'app-help-guide',
   standalone: true,
-  imports: [],
+  imports: [SharedModule,NgbModule],
   templateUrl: './help-guide.component.html',
   styleUrl: './help-guide.component.scss'
 })
 export class HelpGuideComponent {
+  defaults = ['Collapse Group Item #1','Collapse Group Item #2','Collapse Group Item #3'];
 
 }
