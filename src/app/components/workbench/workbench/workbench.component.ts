55f79d626cec094346ef47929fc7b0851e5d6b3c
Narendra Vaila <vnarendra@stratapps.com>
2024-11-18 13:04:42 +0530

    connect button disable issue for oracle

--- src/app/components/workbench/workbench/workbench.component.ts
+++ src/app/components/workbench/workbench/workbench.component.ts
@@ -76,6 +76,7 @@ export class WorkbenchComponent implements OnInit{
   totalItems:any;
   fileData:any;
   viewDatasourceList = false;
+  isEditOracle : boolean = false;
   constructor(private modalService: NgbModal, private workbechService:WorkbenchService,private router:Router,private toasterservice:ToastrService,
     private viewTemplateService:ViewTemplateDrivenService,@Inject(DOCUMENT) private document: Document,private loaderService:LoaderService,private cd:ChangeDetectorRef){ 
     localStorage.setItem('QuerySetId', '0');
@@ -779,6 +780,12 @@ export class WorkbenchComponent implements OnInit{
     this.displayName = editData.display_name;
     this.databaseId=editData.database_id;
     this.databaseType = editData.database_type;
+    if(this.databaseType === 'oracle'){
+      this.isEditOracle = true;
+    } else{
+      this.isEditOracle = false;
+    }
+    this.errorCheck();
     }
 
     Openmdo(OpenmdoModal: any) {
@@ -910,10 +917,15 @@ export class WorkbenchComponent implements OnInit{
     this.errorCheck();
   }
   databaseConditionError(){
-    if(this.postGreDatabaseName){
+    if (!this.isEditOracle) {
+      if (this.postGreDatabaseName) {
+        this.databaseError = false;
+      } else {
+        this.databaseError = true;
+      }
+    }
+    else {
       this.databaseError = false;
-    }else{
-      this.databaseError = true;
     }
     this.portConditionError();
     this.errorCheck();
@@ -959,8 +971,14 @@ export class WorkbenchComponent implements OnInit{
   errorCheck(){
     if(this.serverError || this.portError || this.databaseError || this.userNameError || this.displayNameError || this.passwordError){
       this.disableConnectBtn = true;
-    } else if(!(this.postGreServerName && this.postGrePortName && this.postGreDatabaseName && this.postGreUserName && this.displayName && this.PostGrePassword)) {
-      this.disableConnectBtn = true;
+    } else if(!(this.postGreServerName && this.postGrePortName && this.postGreUserName && this.displayName && this.PostGrePassword)) {
+      if(this.isEditOracle){
+        this.disableConnectBtn = true;
+      } else {
+        if(this.postGreDatabaseName){
+          this.disableConnectBtn = true;
+        }
+      }
     } else{
       this.disableConnectBtn = false;
     }
