235e701be651824cc14fe1fe3ebdb9de9c835c6e
Poojitha Bandaru <bpoojitha@stratapps.com>
2025-01-10 15:41:28 +0530

    fixed issue on sametable re-drop in semantic layer

--- src/app/components/workbench/database/database.component.ts
+++ src/app/components/workbench/database/database.component.ts
@@ -841,7 +841,7 @@ buildCustomJoin(){
   this.tableJoiningList =[];
   this.joinTypes.forEach((element : any,index : number) => {
     let object;
-    let remainingTables = this.draggedtables.filter((table: { alias: string; }) => table.alias == this.draggedtables[index + 1].table);
+    let remainingTables = [this.draggedtables[index + 1]];
     if(this.relationOfTables[index] && this.relationOfTables[index].length){
     object = {
        join : element,
