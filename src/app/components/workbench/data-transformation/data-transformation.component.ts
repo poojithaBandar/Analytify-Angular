c72213d9fbc01673082b0367432312db71fc0878
rajeshkumar-stratapps <rajeshraikode@stratapps.com>
2025-02-14 09:36:44 +0530

    IN-343 Transformations Layer html

--- src/app/components/workbench/data-transformation/data-transformation.component.ts
+++ src/app/components/workbench/data-transformation/data-transformation.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/sharedmodule';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-data-transformation',
   standalone: true,
-  imports: [],
+  imports: [SharedModule,NgbModule],
   templateUrl: './data-transformation.component.html',
   styleUrl: './data-transformation.component.scss'
 })
 export class DataTransformationComponent {
-
+  active=1;
 }
