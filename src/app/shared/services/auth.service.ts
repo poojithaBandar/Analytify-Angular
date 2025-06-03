96661e979022d8ed9c9e9a334e8864d704f237d8
Poojitha Bandaru <bpoojitha@stratapps.com>
2025-06-03 19:45:32 +0530

    IN-474 redirection token

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -170,7 +170,9 @@ hubspotCallBack(data:any){
   return this.http.post<any>(`${environment.apiUrl}/hubspot_callback/`+this.accessToken,data);
 }
 resetPassword(token:any,data:any){
-  return this.http.put<any>(`${environment.apiUrl}/reset_password/confirm`+'/'+token,data);
+  const currentUser = localStorage.getItem( 'currentUser' );
+  this.accessToken = JSON.parse( currentUser! )['Token'];
+  return this.http.put<any>(`${environment.apiUrl}/reset_password/confirm`+'/'+ this.accessToken,data);
 }
 reactivateEmail(data:any){
   return this.http.post<any>(`${environment.apiUrl}/re_activation`+'/',data);
