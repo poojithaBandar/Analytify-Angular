1ec0831e490f2fc993178847c1f2d61831650e91
poojithaBandar <116359080+poojithaBandar@users.noreply.github.com>
2025-07-16 15:41:09 +0530

    Add protected dashboards route and menu

--- src/app/components/authentication/protected-dashboards/protected-dashboards.component.ts
+++ src/app/components/authentication/protected-dashboards/protected-dashboards.component.ts
@@ -6,26 +6,26 @@ import { SharedModule } from '../../../shared/sharedmodule';
 import { WorkbenchService } from '../../workbench/workbench.service';
 
 @Component({
-  selector: 'app-protected-home',
+  selector: 'app-protected-dashboards',
   standalone: true,
   imports: [CommonModule, RouterModule, NgbModule, SharedModule],
-  templateUrl: './protected-home.component.html',
-  styleUrls: ['./protected-home.component.scss']
+  templateUrl: './protected-dashboards.component.html',
+  styleUrls: ['./protected-dashboards.component.scss']
 })
-export class ProtectedHomeComponent implements OnInit {
+export class ProtectedDashboardsComponent implements OnInit {
   dashboards: any[] = [];
   gridView = true;
 
   constructor(private workbenchService: WorkbenchService, private router: Router) {}
 
   ngOnInit(): void {
-    const email = localStorage.getItem('protected_email');
-    if (!email) {
+    const token = JSON.parse(localStorage.getItem('currentUser') || '{}').Token;
+    if (!token) {
       return;
     }
-    this.workbenchService.getProtectedDashboards(email).subscribe({
+    this.workbenchService.getProtectedDashboardsList(token).subscribe({
       next: (data) => {
-        this.dashboards = (data && (data.dashboards || data)) || [];
+        this.dashboards = data?.sheets || [];
       },
       error: () => {}
     });
