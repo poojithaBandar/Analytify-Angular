6d7dccfd3abb915668caf1504063f0b523575d18
Poojitha Bandaru <bpoojitha@stratapps.com>
2025-08-28 16:54:42 +0530

    IN-565 zoomissue fix and hide hierarchy for treemap

--- src/app/components/workbench/insight-apex/insight-apex.component.ts
+++ src/app/components/workbench/insight-apex/insight-apex.component.ts
@@ -5,10 +5,6 @@ import { ChartComponent, NgApexchartsModule } from 'ng-apexcharts';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { SharedModule } from '../../../shared/sharedmodule';
 import _ from 'lodash';
-import { fontWeight } from 'html2canvas/dist/types/css/property-descriptors/font-weight';
-import { offset } from '@popperjs/core';
-import { text } from 'd3';
-import { CommonModule } from '@angular/common';
 
 interface Dimension {
   name: string;
@@ -18,7 +14,7 @@ interface Dimension {
 @Component({
   selector: 'app-insight-apex',
   standalone: true,
-  imports: [SharedModule, NgxEchartsModule, NgSelectModule,NgbModule,CommonModule,
+  imports: [SharedModule, NgxEchartsModule, NgSelectModule,NgbModule,
     NgApexchartsModule
     ],
   templateUrl: './insight-apex.component.html',
